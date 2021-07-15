cd187de9307082a34b0203642dfd141c70f19414
Matthew Pawlak <pharaohnof@gmail.com>
2021-07-15 03:16:20 -0400

    updates

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const uri = `mongodb+srv://pharaohnof:${process.env.DB_PW}@cluster0.9js9i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://pharaohnof:jennY8675309@cluster0.9js9i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 
 
